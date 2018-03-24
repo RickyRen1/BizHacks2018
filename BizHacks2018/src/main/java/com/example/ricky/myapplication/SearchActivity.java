5f2c1da8a54fa84c8b7b98779d68ab5d5706b46d
RickyRen1 <rickyrenx@gmail.com>
2018-03-24 01:08:51 -0700

    added listView to represent search results

--- BizHacks2018/src/main/java/com/example/ricky/myapplication/SearchActivity.java
+++ BizHacks2018/src/main/java/com/example/ricky/myapplication/SearchActivity.java
@@ -6,13 +6,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class SearchActivity extends AppCompatActivity {
 
+    ListView list;
+    String productList[] = { "computer", "tv", "mouse", "games", "asd", "sdf", "sdfds", "asd", "sdf", "sdfds", "asd", "sdf", "sdfds", "asd", "sdf", "sdfds", "asd", "sdf", "sdfds", "asd", "sdf", "sdfds" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        list = (ListView)findViewById(R.id.listView);
+        System.out.println("Creating arrayAdapter");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, productList);
+        System.out.println("Setting arrayAdapter to list");
+        list.setAdapter(arrayAdapter);
 
     }
 
