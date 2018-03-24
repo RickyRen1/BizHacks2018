eacf1261d410af2034b99942e000cc4c850e97da
RickyRen1 <rickyrenx@gmail.com>
2018-03-24 10:24:15 -0700

    prototype push notification

--- BizHacks2018/src/main/java/com/example/ricky/myapplication/MainActivity.java
+++ BizHacks2018/src/main/java/com/example/ricky/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ricky.myapplication;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -58,6 +61,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         );
+
+        NotificationManager notif = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notify=new Notification.Builder
+                (getApplicationContext()).setContentTitle("Sale!").setContentText("Product goes here").
+                setContentTitle("Subject goes here").setSmallIcon(R.drawable.bestbuy_logo).build();
+        notif.notify(0, notify);
 
     }
 }
