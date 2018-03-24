2f86d281561db4fa2b0958154d141186da0367fd
RickyRen1 <rickyrenx@gmail.com>
2018-03-24 15:23:21 -0700

    set favourite toggles sorta

--- BizHacks2018/src/main/java/com/example/ricky/myapplication/SearchActivity.java
+++ BizHacks2018/src/main/java/com/example/ricky/myapplication/SearchActivity.java
@@ -174,14 +174,29 @@ public class SearchActivity extends AppCompatActivity {
         System.out.println("Creating arrayAdapter");
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, productList);
         System.out.println("Setting arrayAdapter to list");
+//        ArrayList<Item> items = itemList.getArrayItem();
         list.setAdapter(arrayAdapter);
         list.setItemsCanFocus(false);
         list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        for(int i=0; i<arrItemList.size(); i++) {
+            System.out.println(arrItemList.get(i));
+            System.out.println(arrItemList.get(i).getName() + " favourite = " + arrItemList.get(i).getFavourite());
+            if(arrItemList.get(i).getFavourite() == true) {
+                list.setItemChecked(i, true);
+            }
+        }
+//        list.setItemChecked(0, true);
+//        list.setItemChecked(1, true);
+//        list.setItemChecked(2, true);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView list, View view, int position, long id) {
-                System.out.println(id);
-                System.out.println(productList.get((int) id));
+                //ArrayList<Item> items = itemList.getArrayItem();
+                System.out.println("item id is " + id);
+                System.out.println("item position is " + position);
+                System.out.println("item name is " + arrItemList.get((int)id).getName() + " using id");
+                System.out.println("item name is " + arrItemList.get(position).getName() + " using position");
+                arrItemList.get(position).setFavourite(!(arrItemList.get(position).getFavourite()));
 
             }
         });
